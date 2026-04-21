4f13ddfc60ef3ed4aa0227c9f29613492aef656c
Sandip 1401 <sandipsongra14@gmail.com>
2026-04-21 15:38:44 +0900

    Connect to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { PORT } from './config/env.js'
+import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 
@@ -7,8 +8,10 @@ app.get('/', (req, res) => {
    res.send("Welcome to SubApp")
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
    console.log(`Server is running on http://localhost:${PORT}`)
+   
+   await connectToDatabase();
 })
 
 export default app;
