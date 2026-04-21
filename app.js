3918767dd5607abe27d411b003349a5a2daa47bb
Sandip 1401 <sandipsongra14@gmail.com>
2026-04-21 18:46:31 +0900

    commit remaining file in other folder

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import { PORT } from './config/env.js'
 import connectToDatabase from './database/mongodb.js';
+import errorMiddlerware from './middleware/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser())
+app.use(errorMiddlerware)
 
 app.get('/', (req, res) => {
    res.send("Welcome to SubApp")
